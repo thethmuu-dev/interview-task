4f5628c9c1017c8c2f2af379271f0810f80906f8
thethmuu-dev <richardfromsv@gmail.com>
2020-09-09 13:31:12 +0630

    Added Validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,9 @@
 class Property < ApplicationRecord
-    has_many :stations
-    accepts_nested_attributes_for :stations
+    validates :name, presence: true
+    validates :rent_fee, presence: true
+    validates :address, presence: true
+    validates :build_year, presence: true
+    
+    has_many :stations, dependent: :destroy
+    accepts_nested_attributes_for :stations, allow_destroy: true
 end
