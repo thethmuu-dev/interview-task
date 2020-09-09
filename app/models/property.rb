14780962ed1b9522fb8a054e93e99189e27dd188
Thet Hmuu Eain Soe <68364897+thethmuu-dev@users.noreply.github.com>
2020-09-09 15:26:11 +0630

    Update property.rb

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,7 @@ class Property < ApplicationRecord
     validates :rent_fee, presence: true
     validates :address, presence: true
     validates :build_year, presence: true
+    validates :remark, presence: true
 
     has_many :stations, dependent: :destroy
     accepts_nested_attributes_for :stations, reject_if: :all_blank, allow_destroy: true
